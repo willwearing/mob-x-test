0b539943eea062c6bd7aad6e8e141384d3236226
willwearing <willwearing@gmail.com>
2021-03-19 12:32:13 -0600

    ant designs and mobx-react installed, ant designs working. note: need to import antd/dist/antd.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import "antd/dist/antd.css";
+
+//import components
+import ButtonTest from "./components/ButtonTest";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Space X Mission List</header>
+      <header className="App-header">
+        <h1 className="app-title">Space X Mission List</h1>
+        <ButtonTest />
+      </header>
     </div>
   );
 }
---
+++ src/components/ButtonTest.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+const ButtonTest = () => {
+  return (
+    <div>
+      <Button type="primary">Fetch</Button>
+    </div>
+  );
+};
+
+export default ButtonTest;
